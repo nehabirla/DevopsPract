ca2915959f3b20554bdcaea1ad43a0760060b161
nehabirla <nbirla@tibco.com>
2021-08-02 21:31:25 +0530

    Adding the project

---
+++ src/test/java/QAMavenGroup/QAMavenArtifact/AppTest.java
@@ -0,0 +1,21 @@
+package QAMavenGroup.QAMavenArtifact;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("Hi this is java");
+    }
+}
